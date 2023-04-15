d0c688d4fec39b68c4b367809afe6f2d20c4e631
henrikhildre <henrikhildre@gmail.com>
2023-04-15 21:32:24 +0200

    Finish 05 and 60

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,39 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    console.log("The numbers are " + a + " and " + b);
+    let sum = 0;
+//    if (a > b) {
+//        console.log(b + " is smaller than " + a);
+//        sum = 'ERROR';
+//        console.log(sum);
+//        return sum;
+//    };
+    console.log("A is a number?" + !Number.isNaN(a) + ". And is B a number? " + !Number.isNaN(b));
+    if (a < 0 || typeof a !== 'number' || b < 0 || typeof b !== 'number'){
+        console.log("Returning error");
+        return 'ERROR';
+    };
+    let smallNum;
+    let largeNum;
+    if (a > b){
+        console.log("A is biggest.");
+        smallNum = b;
+        largeNum = a;
+        console.log("smallNum is " + smallNum + " and largeNum is " + largeNum);
+    } else {
+        console.log("B is biggest.");
+        smallNum = a;
+        largeNum = b;
+        console.log("smallNum is " + smallNum + " and largeNum is " + largeNum);
+    };
+    console.log("Done with if");
+    
+    for (let i = smallNum; i <= largeNum; i++) {
+        console.log("Starting at the top. The counter is on " + i);
+        sum += i;
+        console.log("The sum is at " + sum);
+    };
+    console.log("That's the answer. " + sum + ".");
+    return sum;
 };
 
 // Do not edit below this line
