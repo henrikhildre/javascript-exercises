edd20e7a74d06d9420368d304e9d3d8351d07aed
henrikhildre <henrikhildre@gmail.com>
2023-04-16 07:26:59 +0200

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,6 @@
 const palindromes = function (str) {
-    let string = str.toLowerCase().replaceAll('.,! ','');
+    let string = str.toLowerCase().replaceAll(/[!., ]/g,'');
     let string2 = string.split("").reverse().join("");
-//need to do this with a regex search for the replacement 
     return string == string2;
 };
 
